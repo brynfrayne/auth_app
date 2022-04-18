ec3ef30aff7166a5bcc7a0db1152e9c6be1aba44
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-04-18 15:44:56 -0700

    added productin object to knex file

--- server/knexfile.js
+++ server/knexfile.js
@@ -10,5 +10,14 @@ module.exports = {
       database: 'auth_app',
       charset: 'utf8',
     },
+  },
+  production: {
+    client: 'mysql',
+    connection: {
+      host: 'us-cdbr-east-05.cleardb.net',
+      user: 'be12b40c26e091',
+      password: '11ce8dd3',
+      database: 'heroku_8851603c56e840a'
+    }
   }
 };
